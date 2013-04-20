125a43b9e75e34f549fee3c759d3662ccfa6f00f
Lucas Neves Martins <lucasmartins@railsnapraia.com>
2013-04-19 23:31:10 -0300

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNaPraiaDuvidas::Application.config.secret_token = '4fe9d938f0f4cd58afdda42be2e5a4d280ab80b9deb6e26e3a9c5538fcab430b98b754c117db945ade692419ed928323893da0c43b94c2ab9eac4e9aa9a65e29'
